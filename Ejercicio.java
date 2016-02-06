bcb384cb688804263f16ed0e978e122a42f93767
Luis Cortés <luizcortesj@gmail.com>
2016-02-06 17:07:39 -0600

    Create Ejercicio.java

---
+++ Ejercicio.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio;
+
+/**
+ *
+ * @author laboratorio
+ */
+import java.util.Scanner;
+public class Ejercicio {
+    static Scanner entrada = new Scanner(System.in);
+    static public int valor = 0;
+    static public int b =0;
+    static public int c =0;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("DIGITE UN VALOR");
+        valor = entrada.nextInt();
+        for(int v = 0; v < 10; v++ ){
+            System.out.println(v);
+            if( v == 4){
+                b = valor + v;
+                System.out.println("valor es ("+b+")");
+            }
+            if(v == 7){
+                c = valor + v;
+                System.out.println("valor es ("+c+")");
+            }
+            if(v == 9){
+                int d = b + c;
+                System.out.println("la suma de los valores es ("+d+")");
+            }
+        }
+    }
+    
+}

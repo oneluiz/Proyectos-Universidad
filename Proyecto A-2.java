ba0ef88b2af2c8de1c97325f7a6b1e31b9e845a2
Luis Cortés <luizcortesj@gmail.com>
2016-03-18 10:40:38 -0600

    Update Proyecto A-2.java

--- Proyecto A-2.java
+++ Proyecto A-2.java
@@ -9,58 +9,38 @@ public class Proyecto {
 
     public static void main(String[] args) {
         // variables
-        String dia          = "";
-        int DiaInt          = 1;
-        int semana          = 7;
-        int mes             = 30;
-        int anio            = 365;
+        int dia                     = 0;
+        int semana                  = 7;
+        int mes                     = 30;
+        int anio                    = 365;
         int ResultadoSemanaDivision = 0;
         int ResultadoMesDivision    = 0;
         int ResultadoAnioDivision   = 0;
         //Propiedad del sistema para salto de línea:
         String NuevaLinea = System.getProperty("line.separator");
-        
+
         do{
             try{
-                dia = JOptionPane.showInputDialog("Digite los días que desea validar", null);
-                DiaInt = StringAentero(dia);
-                if(DiaInt < semana ){
+                dia = Integer.parseInt(JOptionPane.showInputDialog("Digite los días que desea validar",null));
+                if(dia < semana ){
                     JOptionPane.showMessageDialog(null, "El número de dias ingresado es menor a la cantidad permitida, la cantidad minima es 7.", "No puedes Ingresar esa cantidad de días", JOptionPane.WARNING_MESSAGE);
                 }else{
-                    ResultadoSemanaDivision = DiaInt/semana;
-                    ResultadoMesDivision    = DiaInt/mes;
-                    ResultadoAnioDivision   = DiaInt/anio;
-                    int ResultadoSemana     = (DiaInt >= semana)? ResultadoSemanaDivision : 0 ;
-                    int ResultadoMes        = (DiaInt >= semana)? ResultadoMesDivision : 0 ;
-                    int ResultadoAnio       = (DiaInt >= semana)? ResultadoAnioDivision : 0 ;
+                    ResultadoSemanaDivision = dia/semana;
+                    ResultadoMesDivision    = dia/mes;
+                    ResultadoAnioDivision   = dia/anio;
+                    int ResultadoSemana     = (dia >= semana)? ResultadoSemanaDivision : 0 ;
+                    int ResultadoMes        = (dia >= semana)? ResultadoMesDivision : 0 ;
+                    int ResultadoAnio       = (dia >= semana)? ResultadoAnioDivision : 0 ;
                     JOptionPane.showMessageDialog(null,"Se obtuvo el siguiente resultado:"+NuevaLinea+"Semana: "+ResultadoSemana+ NuevaLinea + "Mes: "+ResultadoMes+ NuevaLinea + "Año: "+ResultadoAnioDivision, "Resultado de lo que solicitas", JOptionPane.PLAIN_MESSAGE);
                 }
-            }catch(Exception e){
-                JOptionPane.showMessageDialog(null, "El número de dias ingresado es menor a la cantidad permitida, la cantidad minima es 7.", "Seguro que deseas salir de la aplicación", JOptionPane.ERROR_MESSAGE);
-                System.exit(0);
+            }catch(NumberFormatException e ){
+                JOptionPane.showMessageDialog(null, "Debe de ingresar solo caracteres numericos.", "Seguro que deseas salir de la aplicación", JOptionPane.ERROR_MESSAGE);
+                
+                // Solicitud para salir del sistema
+                if (JOptionPane.showConfirmDialog(null, "¿Quieres Salir del sistema?", "Salir del Sistema", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+                    System.exit(0);
+                }
             }
-        }while(DiaInt < 7 );
-    }
-
-    public static int StringAentero( String str ){
-        int i = 0;
-        int num = 0;
-        boolean EsNegativo = false;
-
-        //Compruebe si hay signo negativo; si está allí, establecer el indicador isNeg
-        if (str.charAt(0) == '-') {
-            EsNegativo = true;
-            i = 1;
-        }
-
-        //Procesar cada carácter de la cadena;
-        while( i < str.length()) {
-            num *= 10;
-            num += str.charAt(i++) - '0';
-        }
-
-        if (EsNegativo)
-            num = -num;
-        return num;
+        }while(dia < 7 );
     }
 }

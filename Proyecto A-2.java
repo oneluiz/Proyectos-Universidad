ae8fddf63fe0866d06ba235b7b398577c5c915fe
Luis Cortés <luizcortesj@gmail.com>
2016-03-19 12:43:13 -0600

    Update Proyecto A-2.java

--- Proyecto A-2.java
+++ Proyecto A-2.java
@@ -21,18 +21,21 @@ public class Proyecto{
 
         do{
             try{
-                dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite los días que desea validar", "ULatina | Luis Cortés", JOptionPane.PLAIN_MESSAGE));
-                if(dia < semana ){
-                    JOptionPane.showMessageDialog(null, "El número de dias ingresado es menor a la cantidad permitida, la cantidad minima es 7.", "No puedes Ingresar esa cantidad de días", JOptionPane.WARNING_MESSAGE);
-                }else{
-                    ResultadoSemanaDivision = dia/semana;
-                    ResultadoMesDivision    = dia/mes;
-                    ResultadoAnioDivision   = dia/anio;
-                    int ResultadoSemana     = (dia >= semana)? ResultadoSemanaDivision : 0 ;
-                    int ResultadoMes        = (dia >= semana)? ResultadoMesDivision : 0 ;
-                    int ResultadoAnio       = (dia >= semana)? ResultadoAnioDivision : 0 ;
-                    JOptionPane.showMessageDialog(null,"Se obtuvo el siguiente resultado:"+NuevaLinea+"Semana: "+ResultadoSemana+ NuevaLinea + "Mes: "+ResultadoMes+ NuevaLinea + "Año: "+ResultadoAnioDivision, "Resultado de lo que solicitas", JOptionPane.PLAIN_MESSAGE);
-                }
+                boolean salir = false;
+                do{
+                    dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite los días que desea validar", "ULatina | Luis Cortés", JOptionPane.PLAIN_MESSAGE));
+                    if(dia < semana ){
+                        JOptionPane.showMessageDialog(null, "El número de dias ingresado es menor a la cantidad permitida, la cantidad minima es 7.", "No puedes Ingresar esa cantidad de días", JOptionPane.WARNING_MESSAGE);
+                    }else{
+                        ResultadoSemanaDivision = dia/semana;
+                        ResultadoMesDivision    = dia/mes;
+                        ResultadoAnioDivision   = dia/anio;
+                        int ResultadoSemana     = (dia >= semana)? ResultadoSemanaDivision : 0 ;
+                        int ResultadoMes        = (dia >= semana)? ResultadoMesDivision : 0 ;
+                        int ResultadoAnio       = (dia >= semana)? ResultadoAnioDivision : 0 ;
+                        JOptionPane.showMessageDialog(null,"Se obtuvo el siguiente resultado:"+NuevaLinea+"Semana: "+ResultadoSemana+ NuevaLinea + "Mes: "+ResultadoMes+ NuevaLinea + "Año: "+ResultadoAnioDivision, "Resultado de lo que solicitas", JOptionPane.PLAIN_MESSAGE);
+                    }
+                }while(!salir);
             }catch(NumberFormatException e ){
                 JOptionPane.showMessageDialog(null, "Debe de ingresar solo caracteres numericos.", "Seguro que deseas salir de la aplicación", JOptionPane.ERROR_MESSAGE);
 
